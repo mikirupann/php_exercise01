f69a9122522565ba2074348e4e074d2086d8b310
mikirupann <sw.miki.0907@icloud.com>
2022-02-25 21:53:24 +0900

    再修正　制御構文1の課題

--- 04/if_5.php
+++ 04/if_5.php
@@ -1,12 +1,13 @@
 <?php
+
 $num = 0;
 
+$flg = true;
+
 if ($num <= 1) {
-    echo "{$num}は素数ではありません。";
-    exit;
+    $flg = false;
 }
 
-$flg = true;
 for ($i = 2; $i < $num; $i++) {
     if ($num % $i == 0) {
         $flg = false;
